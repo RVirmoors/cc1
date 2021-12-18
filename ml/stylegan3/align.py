521064659354218ab0f20b274850bae8b525827a
RVirmoors <prowlergrig@yahoo.ie>
2021-12-18 23:09:13 +0200

    projection work

---
+++ ml/stylegan3/align.py
@@ -0,0 +1,21 @@
+import time
+import dlib # requires cmake
+import numpy as np
+from align_face import align_face
+from Library.Spout import Spout
+
+spout = Spout(silent = False, width = 1044, height = 1088)
+spout.createReceiver('input')
+spout.createSender('output')
+
+python projector.py --target=out/seed0002.png --project-in-wplus --save-video --num-steps=1000 --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl
+
+while True :
+    # check on close window
+    spout.check()
+    # receive data
+    data = spout.receive()#.astype(np.uint8)
+    print(data.shape)
+    data = align_face(data)
+    print(data.shape)
+    spout.send(data)
